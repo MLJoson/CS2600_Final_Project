55b45e4e5e6ebc4e9737d60950ac942397e467f2
Enhan540 <147754751+Enhan540@users.noreply.github.com>
2025-05-11 21:16:17 -0700

    Delete temp_var.sh

--- temp_var.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Establishing (temporary) environment variables..."
-
-export PKG_CONFIG_PATH=$HOME/.local/lib/pkgconfig:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH
